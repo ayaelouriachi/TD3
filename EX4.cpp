f6cffa1ed74273c091d389b5991f1667b5e44570
ayaelouriachi <148795694+ayaelouriachi@users.noreply.github.com>
2023-11-04 20:24:00 +0100

    Update EX4.cpp

--- EX4.cpp
+++ EX4.cpp
@@ -2,15 +2,15 @@
 #include<set>
 #include<vector>
 using namespace std;
+bool recherche(set<int>s,int number){
+  if(s.find(number)!=s.end()){
 
-bool recherche(set<int>::iterator begin, set<int>::iterator end, int number) {
-    for (set<int>::iterator it = begin; it != end; ++it) {
-        if (*it == number) {
-            return true;
-        }
-    }
+    return true;
+  }
+
     return false;
 }
+
 
 
 int main() {
@@ -18,7 +18,7 @@ int main() {
 for(int i=0;i<100;i++){
 s.insert(i);}
 int number_research=89;
-if(recherche(s.begin(),s.end(),number_research)==true){
+if(recherche(s,number_research)==true){
     cout<<"le nombre est existe";}
 else{
     cout<<"le nombre n'exist pas";
